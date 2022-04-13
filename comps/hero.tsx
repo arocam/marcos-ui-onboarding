2186c0e7c70c6e4a09b568cee47dfd5efebed29a
Marcos Alvarez <alvarez2003marcos@gamil.com>
2022-04-12 22:00:57 -0300

    Hero started. ArrowVector Component created.

---
+++ comps/hero.tsx
@@ -0,0 +1,11 @@
+import { arrowVector } from "./arrow-vector"
+import { heroText } from "./hero-text"
+
+export const Hero = () =>{
+    return(
+        <>
+        <h1>HERO</h1>
+            <arrowVector/>
+        </>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,25 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Logo from 'next/image'
+import Image from 'next/image'
+import { NextPage } from 'next'
+import ArrowVector from '../comps/arrow-vector'
 
-export default function Home() {
+const Home: NextPage = () => {
   return (
     <div>
+      {/* HEAD */}
+      <Head>
+        <title>UI Onboarding</title>
+      </Head>
+      {/* HEADER */}
       <header className={styles.header}>
-        <Logo className={styles.logo} src="/img/aluxion.png" height={24} width={133}></Logo>
+        <Image className={styles.logo} src="/img/aluxion.png" height={24} width={133}></Image>
       </header>
+      {/* HERO */}
+      <div className={styles.hero}>
+        <ArrowVector></ArrowVector>
+      </div>
     </div>
   )
-}
+};
+export default Home;
