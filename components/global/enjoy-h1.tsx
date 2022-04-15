2e37a0c87e94e91e8a6f5a7d9f03a943f74f856f
Marcos Alvarez <alvarez2003marcos@gamil.com>
2022-04-15 19:42:05 -0300

    Spelling error corrected

--- components/global/enjoy-h1.tsx
+++ components/global/enjoy-h1.tsx
@@ -3,7 +3,7 @@ import styles from './global-components.module.css'
 export const EnjoyH1 = () =>{
     return(
         <>
-            <h1 className={styles.title}>Disfruta de la <br/><span className={styles.c800}> mejor musica</span></h1>
+            <h1 className={styles.title}>Disfruta de la <br/><span className={styles.c800}> mejor música</span></h1>
         </>
     )
 }
