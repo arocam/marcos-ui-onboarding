9082d2c890c9d22884db54e950d918e61bd890c7
Marcos Alvarez <alvarez2003marcos@gamil.com>
2022-04-16 23:19:39 -0300

    Form Styles

--- components/form/form-text.tsx
+++ components/form/form-text.tsx
@@ -3,9 +3,9 @@ import { EnjoyH1 } from "../global/enjoy-h1"
 
 export const FormText = () =>{
     return(
-        <div>
+        <div className={styles.form_text_container}>
             <EnjoyH1></EnjoyH1>
-            <p>Escríbenod en el siguiente formulario y un asesor se pondrá en contacto contigo</p>
+            <p className={styles.paragraph}>Escríbenod en el siguiente formulario y un asesor se pondrá en contacto contigo</p>
         </div>
     )
 }
--- components/form/form.tsx
+++ components/form/form.tsx
@@ -6,8 +6,13 @@ export const Form = () =>{
     return(
         <div className={styles.form_bg}>
             <div className={styles.form_container}>
+                <div className={styles.blue}>
                 <FormText></FormText>
+                </div>
+                <div className={styles.red}>
                 <FormInputs></FormInputs>
+                </div>
+
             </div>
         </div>
     )
