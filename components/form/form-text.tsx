17b2cab372e1ca8ca011e9efa5ba21c44624bd45
Marcos Alvarez <alvarez2003marcos@gamil.com>
2022-04-18 01:37:13 -0300

    Form section paragraph typing error fixed, color and font family changed

--- components/form/form-text.tsx
+++ components/form/form-text.tsx
@@ -5,7 +5,7 @@ export const FormText = () =>{
     return(
         <div className={styles.form_text_container}>
             <EnjoyH1></EnjoyH1>
-            <p className={styles.paragraph}>Escríbenod en el siguiente formulario y un asesor se pondrá en contacto contigo</p>
+            <p className={styles.paragraph}>Escríbenos en el siguiente formulario y un asesor se pondrá en contacto contigo</p>
         </div>
     )
 }
