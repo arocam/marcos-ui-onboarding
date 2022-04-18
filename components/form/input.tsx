94be08a32246ee585a9077d02cc87a0d5961fc59
Marcos Alvarez <alvarez2003marcos@gamil.com>
2022-04-18 14:48:31 -0300

    Input styled component created

--- components/form/input.tsx
+++ components/form/input.tsx
@@ -1,3 +1,18 @@
-export const Input = () =>{
-    
+import styled from 'styled-components'
+
+export const Input = styled.input`
+    width: 100%;
+    max-width: 492px;
+    height: 40px;
+    background-color: transparent;
+    border: none;
+    outline: none;
+    font-size: 24px;
+    margin-bottom:7px;
+
+    /* color: #767676; */
+    color: #fff;
+    :focus{
+    border-bottom: 1px solid #fff;
 }
+`
