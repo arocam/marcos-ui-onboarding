1464155e53811e364712d0332bdcd42278c61f11
Marcos Alvarez <alvarez2003marcos@gamil.com>
2022-04-18 07:08:17 -0300

    Name length checker

--- components/form/form-inputs.tsx
+++ components/form/form-inputs.tsx
@@ -3,42 +3,62 @@ import Link from 'next/link'
 import { useEffect, useRef } from 'react'
 
 export const FormInputs = () =>{
-    const expressions = {
-        usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
-        nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-        password: /^.{4,12}$/, // 4 a 12 digitos.
-        correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-        telefono: /^\d{7,14}$/ // 7 a 14 numeros.
-    }
     const nameInput = useRef();
-    const emailInput = useRef();
-    const reasonInput = useRef();
-    const descriptionInput = useRef();
-    const conditionsInput = useRef();
+    const nameErrorMessageTag = useRef();
+    let nameError = false;
+    const checkNameLength = () =>{
+        let inputName = nameInput;
+        let errorNameMessage = nameErrorMessageTag;
+        if(inputName.current.value.length > 2){
+            inputName.current.style.borderColor = "#FFFFFF";
+            inputName.current.style.color = "#FFFFFF";
+            errorNameMessage.current.style.display = "none"
+            nameError = false;
+        }else{
+            inputName.current.style.borderColor = "#FF7777";
+            inputName.current.style.color = "#FF7777";
+            errorNameMessage.current.style.display = "block"
+            nameError = true;
+        }
+    }
+
     
 
     return(
         <div className={styles.form}>
-            <form className={styles.inputs_container}>
+            <form className={styles.inputs_container} onSubmit={e => e.preventDefault()}>
                 {/* Nombre */}
-                <input className={styles.form_input} type="text" ref={nameInput} placeholder="Nombre"/>
+                <div className={`${styles.input_container} ${styles.input_name_container}`}>
+                    <input ref={nameInput} onKeyUp={checkNameLength} className={styles.form_input} type="text" placeholder="Nombre"/>
+                    <div ref={nameErrorMessageTag} className={styles.error}>El nombre debe tener al menos 3 caracteres</div>
+                </div>
+                
                 {/* Email */}
-                <input className={styles.form_input} type="email" ref={emailInput} placeholder="Email"/>
+                <div className={`${styles.input_container} ${styles.input_email_container}`}>
+                    <input className={styles.form_input} type="email" placeholder="Email"/>
+                </div>
+
                 {/* Razon */}
-                <select className={`${styles.form_input} ${styles.select_input}`} ref={reasonInput} name="" id="">
-                    <option className={`${styles.option__false} ${styles.option}`} value="" disabled selected>Razón</option>
-                    <option className={`${styles.option__true} ${styles.option}`} value="">Colaboración</option>
-                    <option className={`${styles.option__true} ${styles.option}`} value="">Incidencia</option>
-                    <option className={`${styles.option__true} ${styles.option}`} value="">Otros</option>
-                </select>
+                <div className={`${styles.input_container} ${styles.input_reason_container}`}>
+                    <select className={`${styles.form_input} ${styles.select_input}`} name="" id="">
+                        <option className={`${styles.option__false} ${styles.option}`} value="" disabled selected>Razón</option>
+                        <option className={`${styles.option__true} ${styles.option}`} value="">Colaboración</option>
+                        <option className={`${styles.option__true} ${styles.option}`} value="">Incidencia</option>
+                        <option className={`${styles.option__true} ${styles.option}`} value="">Otros</option>
+                    </select>
+                </div>
                 {/* Descripcion */}
-                <textarea className={`${styles.form_input} ${styles.textarea}`} ref={descriptionInput} placeholder="Descripción"></textarea>
+                <div className={`${styles.input_container} ${styles.input_description_container}`}>
+                    <textarea className={`${styles.form_input} ${styles.textarea}`} placeholder="Descripción"></textarea>
+                </div>
                 {/* Terminos y condiciones */}
                 <div className={styles.terms_cond}>
-                    <input ref={conditionsInput} className={styles.checkbox} type="checkbox" /><p className={styles.accept_cond}>Acepto las <span className={styles.link_cond}><Link  href='https://www.spotify.com/es/legal/live-terms/'> condiciones legales</Link></span></p>
+                    <input className={styles.checkbox} type="checkbox" /><p className={styles.accept_cond}>Acepto las <span className={styles.link_cond}><Link  href='https://www.spotify.com/es/legal/live-terms/'> condiciones legales</Link></span></p>
                 </div>
                 {/* Submit */}
-                <input className={`${styles.form_input} ${styles.form_input_submit}`} type="submit" />
+                <div className={`${styles.input_container} ${styles.input_submit_container}`}>
+                    <input className={`${styles.form_input} ${styles.form_input_submit}`} type="submit" />
+                </div>
             </form>
         </div>
     )
