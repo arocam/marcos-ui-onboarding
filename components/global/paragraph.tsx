38c8be2fb7c44c7b3cd90fbc5e3e2de57127544e
Marcos Alvarez <alvarez2003marcos@gamil.com>
2022-04-20 19:00:21 -0300

    Paragraph styled component created

---
+++ components/global/paragraph.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Paragraph = styled.p`
+    font-size: 16px;
+    max-width: 457px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 200%;
+    color: #ffffff;
+    margin-top: 24px;
+`
