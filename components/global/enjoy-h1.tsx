cc90d08f4d9ac2408e05828fca66e241af56df85
Marcos Alvarez <alvarez2003marcos@gamil.com>
2022-04-20 18:56:23 -0300

    Props added to make it dynamic

--- components/global/enjoy-h1.tsx
+++ components/global/enjoy-h1.tsx
@@ -1,9 +1,41 @@
-import styles from './global-components.module.css'
+import styled from 'styled-components'
 
-export const EnjoyH1 = () =>{
+const TitleH1Style = styled.div`
+    .title-white{
+        font-size: 40px;
+        color: #fff;
+        font-weight: 700;
+        line-height: 48px;
+    }
+    .title-800{
+        color: #D6F379;
+    }
+    @media (min-width: 768px){
+      .title-white{
+        font-size: 55px;
+        line-height: 68px;
+      }
+    }
+    @media (min-width: 1200px){
+      .title-white{
+        font-size: 64px;
+        line-height: 78px;
+      }
+    }
+`
+
+
+export const TitleH1 = (props) =>{
+    const {line1, line2} = props;
+
     return(
-        <>
-            <h1 className={styles.title}>Disfruta de la <br/><span className={styles.c800}> mejor música</span></h1>
-        </>
+        <TitleH1Style>
+            <h1 className='title-white'>
+                {line1}<br/>
+                <span className='title-800'>
+                    {line2}
+                </span>
+            </h1>
+        </TitleH1Style>
     )
 }
