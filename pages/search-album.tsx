d91637ab751fb37806fc13f339365ecf4342a399
Marcos Alvarez <alvarez2003marcos@gamil.com>
2022-04-20 18:58:04 -0300

    Body added to this page

--- pages/search-album.tsx
+++ pages/search-album.tsx
@@ -1,10 +1,12 @@
 import { NextPage } from "next"
+import { Body } from "../components/album-search-body/body";
 import { HeaderLogged } from "../components/header-logged/header-logged";
 
 const searchAlbum: NextPage = () =>{
     return(
         <>
             <HeaderLogged currentPage='searchAlbum'></HeaderLogged>
+            <Body></Body>
         </>
     )
 }
