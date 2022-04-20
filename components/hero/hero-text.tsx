c0e183f1ba32def53cc1645d5b890f513698096d
Marcos Alvarez <alvarez2003marcos@gamil.com>
2022-04-20 19:04:27 -0300

    TitleH1 props added at component Hero

--- components/hero/hero-text.tsx
+++ components/hero/hero-text.tsx
@@ -1,9 +1,10 @@
 import styles from './hero.module.css'
-import { EnjoyH1 } from '../global/enjoy-h1';
+import { TitleH1 } from '../global/enjoy-h1';
 import Link from 'next/link';
 import { useRef, useEffect } from 'react';
 import gsap from 'gsap';
 import ScrollToPlugin from 'gsap/dist/ScrollToPlugin';
+import {Paragraph} from '../global/paragraph'
 
 gsap.registerPlugin(ScrollToPlugin);
 
@@ -21,22 +22,14 @@ const HeroText = (props) =>{
     return(
         <div className={styles.hero_text__container}>
             <div className={styles.hero_text}>
-                {/* Disfruta de la mejor musica */}
-                <EnjoyH1></EnjoyH1>
-            
-                {/* Accede a tu cuenta para guardar tus albumes favoritos */}
-                <p className={styles.paragraph}>Accede a tu cuenta para guardar tus <br/> álbumes favoritos</p>
-            
-                {/* LINK TO CONTACT */}
-                
-                {/* <Link href=""> */}
+                <TitleH1 line1='Disfruta de la' line2='mejor música'></TitleH1>
+                <Paragraph>Accede a tu cuenta para guardar tus álbumes favoritos</Paragraph>
                 <div onClick={goToLink} className={styles.link_container} >
                     <p className={styles.link_desc}>{linkName}</p>
                     <svg className={styles.link_vector} width="18" height="16" viewBox="0 0 18 16" fill="none" xmlns="http:www.w3.org/2000/svg">
                         <path d="M10 1L17 8L10 15M1 8H17H1Z" stroke="white"/>
                     </svg>
                 </div>
-                {/* </Link> */}
                 
             </div>
         </div>
