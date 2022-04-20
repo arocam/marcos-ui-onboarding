caebec38d1ad94408b478994e3dbe59282c83f10
Marcos Alvarez <alvarez2003marcos@gamil.com>
2022-04-20 05:59:53 -0300

    Helvetica Neue font added

---
+++ styles/typography.ts
@@ -0,0 +1,46 @@
+import { css } from 'styled-components'
+
+export const Fonts = css`
+    @font-face{
+        font-family: "helvetica-neue";
+        src: url("/assets/fonts/helvetica-neue-bold.otf");
+        font-weight: 600;
+        font-style: normal;
+        font-display: fallback;
+    }
+    @font-face{
+        font-family: "helvetica-neue";
+        src: url("/assets/fonts/helvetica-neue-medium.otf");
+        font-weight: 500;
+        font-style: normal;
+        font-display: fallback;
+    }
+    @font-face{
+        font-family: "helvetica-neue";
+        src: url("/assets/fonts/helvetica-neue.otf");
+        font-weight: 400;
+        font-style: normal;
+        font-display: fallback;
+    }
+    @font-face{
+        font-family: "helvetica-neue";
+        src: url("/assets/fonts/helvetica-neue-regular-italic.otf");
+        font-weight: 400;
+        font-style: normal;
+        font-display: fallback;
+    }
+    @font-face{
+        font-family: "helvetica-neue";
+        src: url("/assets/fonts/helvetica-neue-light.otf");
+        font-weight: 300;
+        font-style: normal;
+        font-display: fallback;
+    }
+    @font-face{
+        font-family: "helvetica-neue";
+        src: url("/assets/fonts/helvetica-neue-thin.otf");
+        font-weight: 200;
+        font-style: normal;
+        font-display: fallback;
+    }
+`
