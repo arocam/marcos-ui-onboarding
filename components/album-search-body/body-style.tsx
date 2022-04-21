2594b18eaa149cca68ac80447a0d426f05ebf02b
Marcos Alvarez <alvarez2003marcos@gamil.com>
2022-04-21 11:47:27 -0300

    Width changed

--- components/album-search-body/body-style.tsx
+++ components/album-search-body/body-style.tsx
@@ -1,12 +1,11 @@
 import styled from 'styled-components'
 
 export const BodyStyle = styled.div`
-    width: 84%;
+    width: 90%;
     height: 100px;
     margin: 0 auto;
     .body-text-search{
         margin-top: 50px;
-        background-color: red;
         text-align: left;
     }
     @media (min-width: 768px){
@@ -14,13 +13,16 @@ export const BodyStyle = styled.div`
             margin-top: 80px;
             text-align: center;
             display: flex;
-        flex-direction: column;
-        align-items : center;
+            flex-direction: column;
+            align-items : center;
         }
     }
     @media (min-width: 992px){
         .body-text-search{
             margin-top: 50px;
         }
+    }
+    @media (min-width: 1360){
+        margin: 85%
     }
 `
