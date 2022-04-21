4b3976a7ac03c2c1a26e8dd91a0cd99b9ee03fea
Marcos Alvarez <alvarez2003marcos@gamil.com>
2022-04-21 11:49:43 -0300

    Albums with objects arrays created

---
+++ public/assets/helpers/albums.tsx
@@ -0,0 +1,27 @@
+export const Albums = {
+    addedAlbumes:[
+        
+    ],
+    albumes:[
+        {id:'a01',albumImageSrc:'01.png', albumName:'Album piola 01', publishedDate: '7/6/2005', added: false},
+        {id:'a02',albumImageSrc:'02.png', albumName:'Album piola 02', publishedDate: '7/6/2005', added: false},
+        {id:'a03',albumImageSrc:'03.png', albumName:'Album piola 03', publishedDate: '7/6/2005', added: false},
+        {id:'a04',albumImageSrc:'04.png', albumName:'Album piola 04', publishedDate: '7/6/2005', added: false},
+        {id:'a05',albumImageSrc:'01.png', albumName:'Album piola 05', publishedDate: '7/6/2005', added: false},
+        {id:'a06',albumImageSrc:'02.png', albumName:'Album no tanpiola 06', publishedDate: '7/6/2005', added: false},
+        {id:'a07',albumImageSrc:'03.png', albumName:'Album re piola 07', publishedDate: '7/6/2005', added: false},
+        {id:'a08',albumImageSrc:'04.png', albumName:'Album piola 08', publishedDate: '7/6/2005', added: false},
+        {id:'a09',albumImageSrc:'01.png', albumName:'Album piola 09', publishedDate: '7/6/2005', added: false},
+        {id:'a10',albumImageSrc:'02.png', albumName:'Album piola 10', publishedDate: '7/6/2005', added: false},
+        {id:'a11',albumImageSrc:'03.png', albumName:'Album piola 11', publishedDate: '7/6/2005', added: false},
+        {id:'a12',albumImageSrc:'04.png', albumName:'Album piola 12', publishedDate: '7/6/2005', added: false},
+        {id:'a13',albumImageSrc:'01.png', albumName:'Album piola 13', publishedDate: '7/6/2005', added: false},
+        {id:'a14',albumImageSrc:'02.png', albumName:'Album piola 14', publishedDate: '7/6/2005', added: false},
+        {id:'a15',albumImageSrc:'03.png', albumName:'Album piola 15', publishedDate: '7/6/2005', added: false},
+        {id:'a16',albumImageSrc:'04.png', albumName:'Album piola 16', publishedDate: '7/6/2005', added: false},
+        {id:'a17',albumImageSrc:'01.png', albumName:'Album piola 17', publishedDate: '7/6/2005', added: false},
+        {id:'a18',albumImageSrc:'02.png', albumName:'Album piola 18', publishedDate: '7/6/2005', added: false},
+        {id:'a19',albumImageSrc:'03.png', albumName:'Album piola 19', publishedDate: '7/6/2005', added: false},
+        {id:'a120',albumImageSrc:'04.png', albumName:'Album piola 20', publishedDate: '7/6/2005', added: false},
+    ]
+}
